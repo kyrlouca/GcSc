a1c9b2636197472e87476c713ad9c2f3cb3ca295
kyrlouca@gmail.com <kyrlouca>
2022-06-14 10:03:39 +0300

    selectoi

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -5,31 +5,32 @@ Module Program
 
     Sub Main()
         Dim myText() = {"3", "8", "5", "4"}
-        Dim sorted = SelectionSort(myText)
+        Dim myNumbers() = {3, 8, 5, 4}
+        Dim sorted = SelectionSort(myNumbers)
 
     End Sub
 
-    Function SelectionSort(text() As String) As String()
-        text = {"3", "8", "5", "4"}
+    Function InsertionSort(text As String()) As String()
+        'text = {"3", "8", "5", "4"}
         Dim textLen = text.Length
-        Dim out(0 To textLen - 1) As String
+        'Dim out(0 To textLen - 1) As String
 
         For i As Integer = 0 To textLen - 1 Step 1
 
-            out(i) = text(i)
+            'out(i) = text(i)
 
             For j As Integer = i To 1 Step -1
-                Console.WriteLine($"out :{String.Join(",", out)}")
-                If (out(j) < out(j - 1)) Then
-                    Swap(out(j), out(j - 1))
+                Console.WriteLine($"out :{String.Join(",", text)}")
+                If (text(j) < text(j - 1)) Then
+                    Swap(text(j), text(j - 1))
                 End If
             Next
 
         Next
 
         Console.WriteLine($"text:{String.Join(",", text)}")
-        Console.WriteLine($"out :{String.Join(",", out)}")
-        Return out
+        'Console.WriteLine($"out :{String.Join(",", out)}")
+        Return text
     End Function
 
 
@@ -38,5 +39,25 @@ Module Program
         a = b
         b = temp
     End Sub
+
+
+    Function SelectionSort(userNumbers As Integer()) As Integer()
+        userNumbers = {4, 8, 5, 3}
+
+        Dim userLength As Integer = userNumbers.Length
+
+        For i As Integer = 0 To userLength - 1
+
+            For j As Integer = i + 1 To userLength - 1
+                Dim userText = $"{String.Join(",", userNumbers)}"
+                If userNumbers(j) < userNumbers(i) Then
+                    Swap(userNumbers(j), userNumbers(i))
+                End If
+            Next
+
+        Next
+        Return userNumbers
+    End Function
+
 
 End Module
