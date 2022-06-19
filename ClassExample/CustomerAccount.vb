347cc133d3cffca8303ff4f36c7f1c856fef0e88
kyrlouca@gmail.com <kyrlouca>
2022-06-19 15:24:06 +0300

    abc

--- ClassExample/CustomerAccount.vb
+++ ClassExample/CustomerAccount.vb
@@ -1,40 +1,49 @@
-﻿Public Class CustomerAccount
-    Private Balance As Double
-    Private Id As Integer
-    Private Owner As String
+﻿Public Class BankAccount
+    Private _balance As Double
+    Private ReadOnly id As Integer
+    Private _ownerName As String
 
-    Public Sub New(id As Integer, owner As String)
-        Balance = 0
-        Me.Id = id
-        Me.Owner = owner
+
+    Public Property OwnerName() As String
+        Get
+            Return _ownerName
+        End Get
+        Set(name As String)
+            _ownerName = name
+        End Set
+    End Property
+
+
+    Public Sub New(id As Integer, ownerName As String)
+        _balance = 0
+        Me.id = id
+        _ownerName = ownerName
     End Sub
 
+
     Public Function GetBalance() As Double
-        Return Balance
+        Return _balance
     End Function
 
-    Public Function GetName() As String
-        Return Owner
-    End Function
 
     Function GetId() As String
-        Return Id
+        Return id
     End Function
     Public Function CreditAccount(Amount As Double) As Boolean
         If (Amount <= 0) Then
             Return False
         End If
-        Balance = Balance + Amount
+        _balance = _balance + Amount
         Return True
     End Function
     Public Function DebitAccount(Amount As Double) As Boolean
         If (Amount <= 0) Then
             Return False
         End If
-        If (Balance - Amount < 0) Then
+        If (_balance - Amount < 0) Then
             Return False
         End If
-        Balance = Balance - Amount
+        _balance -= Amount
         Return True
     End Function
 
---
+++ ClassExample/Vehicle.vb
@@ -0,0 +1,49 @@
+﻿Public MustInherit Class Vehicle
+    Public ReadOnly brand As String
+    Protected _basicValue As Integer
+
+    Public Sub New(brand As String, basicValue As Integer)
+        Me.brand = brand
+        If basicValue < 0 Then
+            _basicValue = 0
+        Else
+            _basicValue = basicValue
+        End If
+
+    End Sub
+
+    Public MustOverride Function CalculateCarValue() As Integer
+
+End Class
+
+Public Class Car
+    Inherits Vehicle
+    Private _metallicPaintCost As Integer
+
+    Overrides Function CalculateCarValue() As Integer
+        Return _basicValue + _metallicPaintCost
+    End Function
+
+
+
+    Public Sub New(brand As String, basicCost As Integer, metallicPaintCost As Integer)
+        MyBase.New(brand, basicCost)
+        _metallicPaintCost = metallicPaintCost
+    End Sub
+End Class
+
+
+Public Class Truck
+    Inherits Vehicle
+    Private _maxLoadKg As Integer
+
+    Public Sub New(brand As String, basicCost As Integer, maxLoadKg As Integer)
+        MyBase.New(brand, basicCost)
+        _maxLoadKg = maxLoadKg
+    End Sub
+
+    Public Overrides Function CalculateCarValue() As Integer
+        Return _basicValue + _maxLoadKg * 100
+    End Function
+
+End Class
